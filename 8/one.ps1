7e52ea2731d2086bb7953cecb4a4bd792231a7f2
Fredrik Jönsson <fjollberg@gmail.com>
2022-12-08 16:24:26 +0100

    Minor cleanup.

--- 8/one.ps1
+++ 8/one.ps1
@@ -1,6 +1,6 @@
 $Data = Get-Content ./input.txt
 
-[array[]]$script:grid = [array[]]::new($Data.Length)
+$script:grid = [array[]]::new($Data.Length)
 
 $i = 0
 
@@ -14,21 +14,25 @@ foreach ($line in $Data) {
     $grid[$i++] = $row
 }
 
-function notSeen([int]$tree) {
-    $tree -ge 0
+# Ugly, but first tree in each row always visible, 0 can only be
+# visible on the edges, so negation to mark already found trees works.
+function setSeen([int]$x, [int]$y) {
+    $grid[$y][$x] = -([Math]::abs($grid[$y][$x]))
+}
+
+function notSeen([int]$x, [int]$y) {
+    $grid[$y][$x] -ge 0
 }
 
 function countBackAndForthRow([int] $row) {
-    $count = 0
+    $local:count = 0
 
-    # first tree visible, 0 can only be visible on the
-    # border, so negation should work.
     $h = $grid[$row][0]
 
     for ($i = 1; $i -lt $grid[$row].Length - 1; $i++) {
-        if ((notSeen $grid[$row][$i]) -and ($grid[$row][$i] -gt $h)) {
+        if ((notSeen $i $row) -and ($grid[$row][$i] -gt $h)) {
             Write-Host "found row" $row $i $grid[$row][$i]
-            $grid[$row][$i] = -$grid[$row][$i]
+            setSeen $i $row
             $count++
         }
         $h = [Math]::max([Math]::Abs($grid[$row][$i]), $h)
@@ -37,9 +41,9 @@ function countBackAndForthRow([int] $row) {
     $h = $grid[$row][$grid[$row].Length -1]
 
     for ($i = $grid[$row].Length - 2; $i -ge 1; $i--) {
-        if ((notSeen $grid[$row][$i]) -and ($grid[$row][$i] -gt $h)) {
+        if ((notSeen $i $row) -and ($grid[$row][$i] -gt $h)) {
             Write-Host "found row back" $row $i $grid[$row][$i]
-            $grid[$row][$i] = -$grid[$row][$i]
+            setSeen $i $row
             $count++
         }
         $h = [Math]::max([Math]::Abs($grid[$row][$i]), $h)
@@ -49,15 +53,14 @@ function countBackAndForthRow([int] $row) {
 }
 
 function countBackAndForthCol([int] $col) {
-    $count = 0
-    # first tree visible, 0 can only be visible on the
-    # border, so negation should work.
+    $local:count = 0
+
     $h = $grid[0][$col]
 
     for ($i = 1; $i -lt $grid.Length-1; $i++) {
-        if ((notSeen $grid[$i][$col]) -and ($grid[$i][$col] -gt $h)) {
+        if ((notSeen $col $i) -and ($grid[$i][$col] -gt $h)) {
             Write-Host "found col" $i $col $grid[$i][$col]
-            $grid[$i][$col] = -$grid[$i][$col]
+            setSeen $col $i
             $count++
         }
         $h = [Math]::max([Math]::Abs($grid[$i][$col]), $h)
@@ -66,9 +69,9 @@ function countBackAndForthCol([int] $col) {
     $h = $grid[$grid.Length-1][$col]
 
     for ($i = $grid.Length - 2; $i -ge 1; $i--) {
-        if ((notSeen $grid[$i][$col]) -and ($grid[$i][$col] -gt $h)) {
+        if ((notSeen $col $i) -and ($grid[$i][$col] -gt $h)) {
             Write-Host "found col back" $i $col $grid[$i][$col]
-            $grid[$i][$col] = -$grid[$i][$col]
+            setSeen $col $i
             $count++
         }
         $h = [Math]::max([Math]::Abs($grid[$i][$col]), $h)
@@ -82,18 +85,11 @@ function countBackAndForthCol([int] $col) {
 #}
 
 $total = ($grid.Length * 2) + ($grid[0].Length * 2) - 4
-Write-Host "start:" $count
 
 for ($r = 1; $r -lt $grid.Length-1; $r++) {
-    Write-Host "row:" $r
     $total += countBackAndForthRow $r
 }
-for ($c = 1; $c -lt ($grid[0].Length-1); $c++) {
-    Write-Host "col:" $c
+for ($c = 1; $c -lt $grid[0].Length-1; $c++) {
     $total += countBackAndForthCol $c
 }
 Write-Host "answer:" $total
-
-#for ($r = 0; $r -lt $grid.Length; $r++) {
-#    Write-Host "${r}:" $grid[$r]    
-#}
