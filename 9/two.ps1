5ac8a78943019e06c5bffcaa2e1dbb1e6a3710f9
Fredrik Jönsson <fjollberg@gmail.com>
2022-12-09 12:54:58 +0100

    Day 9 done.

--- 9/two.ps1
+++ 9/two.ps1
@@ -1,13 +1,18 @@
 [CmdletBinding()]
 param (
     [String]$File = "./input.txt",
-    [int] $NumberOfKnots = 2,
-    [int] $Stop = 0
+    [int] $NumberOfKnots = 10,
+    [int] $Stop = 0,
+    [int] $Sleep = 0
 )
 
 $Data = Get-Content $file
 
 $knot = [object[]]::new($NumberOfKnots)
+$max = @{
+    x = 0
+    y = 0
+}
 
 for ($i = 0; $i -lt $NumberOfKnots; $i++) {
     $knot[$i] = @{
@@ -21,46 +26,60 @@ $visited = @{
 }
 
 function shouldMove([int]$k) {
-    ([Math]::Abs($knot[$k].x - $knot[$k+1].x) -gt 1) -or ([Math]::Abs($knot[$k].y - $knot[$k+1].y) -gt 1)
+    ([Math]::Abs($knot[$k+1].x - $knot[$k].x) -gt 1) -or ([Math]::Abs($knot[$k+1].y - $knot[$k].y) -gt 1)
 }
 
 function visit([int]$x, [int]$y) {
     $visited[("{0}:{1}" -f $x, $y)] = 1
-    Write-Verbose ("visited: {0} {1}" -f $x, $y)
+}
+
+function updateMax([int]$x, [int]$y) {
+    $max.x = [Math]::Max($max.x, [Math]::Abs($x))
+    $max.y = [Math]::Max($max.y, [Math]::Abs($y))
 }
 
 function moveRight([int] $step) {
     while ($step--) {
         $knot[$knot.Count-1].x++
+        updateMax $knot[$knot.Count-1].x $knot[$knot.Count-1].y 
 
         for ($k = $knot.Count-2; $k -ge 0; $k--) {
             if (shouldMove $k) {
-                if ($knot[$k+1].y -gt $knot[$k].y) {
-                    $knot[$k].y++
-                } elseif ($knot[$k+1].y -lt $knot[$k].y) {
-                    $knot[$k].y--
-                }
-                $knot[$k].x++
+                moveKnot $k
+            } else {
+                break
             }
         }
         visit $knot[0].x $knot[0].y
     }
 }
 
+function moveknot([int]$k) {
+    if ($knot[$k+1].y -gt $knot[$k].y) {
+        $knot[$k].y++
+    } elseif ($knot[$k+1].y -lt $knot[$k].y) {
+        $knot[$k].y--
+    }
+    if ($knot[$k+1].x -gt $knot[$k].x) {
+        $knot[$k].x++
+    } elseif ($knot[$k+1].x -lt $knot[$k].x) {
+        $knot[$k].x--
+    }
+}
+
 function moveLeft([int] $step) {
     while ($step--) {
         $knot[$knot.Count-1].x--
+        updateMax $knot[$knot.Count-1].x $knot[$knot.Count-1].y 
 
         for ($k = $knot.Count-2; $k -ge 0; $k--) {
             if (shouldMove $k) {
-                if ($knot[$k+1].y -gt $knot[$k].y) {
-                    $knot[$k].y++
-                } elseif ($knot[$k+1].y -lt $knot[$k].y) {
-                    $knot[$k].y--
-                }
-                $knot[$k].x--
+                moveKnot $k
+            } else {
+                break
             }
         }
+
         visit $knot[0].x $knot[0].y
     }
 }
@@ -68,15 +87,13 @@ function moveLeft([int] $step) {
 function moveUp([int] $step) {
     while ($step--) {
         $knot[$knot.Count-1].y++
+        updateMax $knot[$knot.Count-1].x $knot[$knot.Count-1].y 
 
         for ($k = $knot.Count-2; $k -ge 0; $k--) {
             if (shouldMove $k) {
-                if ($knot[$k+1].x -gt $knot[$k].x) {
-                    $knot[$k].x++
-                } elseif ($knot[$k+1].x -lt $knot[$k].x) {
-                    $knot[$k].x--
-                }
-                $knot[$k].y++
+                moveKnot $k
+            } else {
+                break
             }
         }
         visit $knot[0].x $knot[0].y
@@ -86,24 +103,48 @@ function moveUp([int] $step) {
 function moveDown([int] $step) {
     while ($step--) {
         $knot[$knot.Count-1].y--
+        updateMax $knot[$knot.Count-1].x $knot[$knot.Count-1].y 
 
         for ($k = $knot.Count-2; $k -ge 0; $k--) {
             if (shouldMove $k) {
-                if ($knot[$k+1].x -gt $knot[$k].x) {
-                    $knot[$k].x++
-                } elseif ($knot[$k+1].x -lt $knot[$k].x) {
-                    $knot[$k].x--
-                }
-                $knot[$k].y--
+                moveKnot $k
+            } else {
+                break
             }
         }
         visit $knot[0].x $knot[0].y
     }
 }
 
+function point([int]$x, [int]$y) {
+    for ($i = $knot.Count - 1; $i -ge 0; $i--) {
+        if (($knot[$i].x -eq $x) -and ($knot[$i].y -eq $y)) {
+            if ($i -eq ($knot.Count - 1)) {
+                return 'H'
+            }
+            if ($i -eq 0) {
+                return 'T'
+            }
+            return ($NumberOfKnots - $i - 1)
+        }
+    }
+    if ($x -eq 0 -and $y -eq 0) {
+        return 's'
+    }
+    return '.'
+}
 
+function printExample {
+    foreach ($y in (${max}.y..-${max}.y)) {
+        $str = ''
+        foreach ($x in (-${max}.x)..${max}.x) {
+            $str += point $x $y 
+        }
+        Write-Host $str
+    }
+}
 
-$g = 0
+$g = 1
 foreach ($move in $Data) {
     ($dir, [int]$step) = $move.Split(' ')
     Write-Verbose ("move {2}: {0} {1}" -f $dir, $step, $g)
@@ -114,6 +155,12 @@ foreach ($move in $Data) {
         'D' { moveDown $step; break}
     }
     $g++
+
+    if ($VerbosePreference -ne 'SilentlyContinue') {
+        printExample
+        Write-Host ""
+        Start-Sleep -Milliseconds $Sleep
+    }
     if ($Stop -and ($g -gt $Stop)) {break}
 }
 
