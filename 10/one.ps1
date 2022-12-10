a622f2a149afd913f2eb7b5bd8467c4400968a40
Fredrik Jönsson <fjollberg@gmail.com>
2022-12-10 14:04:58 +0100

    Refactor 1 somewhat.

--- 10/one.ps1
+++ 10/one.ps1
@@ -6,46 +6,31 @@ param (
 $Data = Get-Content $file
 
 $X = 1
-$probe = 20
+$probeStart = 20
 $probeInc = 40
 $cycle = [System.Collections.ArrayList]@()
 $cycle.Add($X) | Out-Null
 
 foreach ($line in $Data) {
-    $command = ''
-    [int]$arg = 0
+    Write-Verbose $line
 
     switch -Regex ($line) {
         "^noop$" {
-            $command = "noop"
-            break
+            $cycle.Add($X) | Out-Null
         }
         "^addx .*" {
             ($command, [int]$arg) = $line.Split(' ')
-            break
-        }
-    }    
-
-    Write-Verbose ("command: {0} arg: {1}" -f $command, $arg)
-
-    switch -Exact ($command) {
-        'noop' {
-            $cycle.Add($X) | Out-Null
-            break;
-        }
-        'addx' {
             $cycle.Add($X) | Out-Null
             $cycle.Add($X) | Out-Null
             $X += $arg
-            break;
         }
-    }
+    }    
 }
 
 $sum = 0
-for ($p = $probe; $p -lt $cycle.Count; $p += $probeInc) {
+for ($p = $probeStart; $p -lt $cycle.Count; $p += $probeInc) {
     $val = $p * $cycle[$p]
     $sum += $val
-    Write-Verbose ("probe: {0} X: {1} val: {2} sum: {3}" -f $p, $cycle[$probe], $val, $sum)
+    Write-Verbose ("probe: {0} X: {1} val: {2} sum: {3}" -f $p, $cycle[$p], $val, $sum)
 }
 Write-Output ("answer: {0}" -f $sum)
